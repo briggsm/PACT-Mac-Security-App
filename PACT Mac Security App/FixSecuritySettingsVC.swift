eb8b7a2c89365fd76daaca90bfd840acacc86f72
Mark Briggs <briggsm@alumni.msoe.edu>
2016-11-17 15:28:14 +0300

    Refactored code to make it more readable and user-friendly.

--- PACT Mac Security App/FixSecuritySettingsVC.swift
+++ PACT Mac Security App/FixSecuritySettingsVC.swift
@@ -1,5 +1,5 @@
 //
-//  FitIt3VC.swift
+//  FixSecuritySettingsVC.swift
 //  PACT Mac Security App
 //
 //  Created by Mark Briggs on 11/14/16.
@@ -10,12 +10,13 @@ import Cocoa
 
 class FixSecuritySettingsVC: NSViewController {
 
+    let settingsToQuery = ["screensaver5sec.sh", "screensaver10min.sh"]
+    
     @IBOutlet weak var settingsStackView: NSStackView!
     @IBOutlet weak var fixAllBtn: NSButton!
     
-    let settingsToQuery = ["screensaver5sec.sh", "screensaver10min.sh"]
-    
     override func viewDidAppear() {
+        // Add (Version Number) to title of Main GUI's Window
         let appName = Bundle.main.infoDictionary![kCFBundleNameKey as String] as! String
         let appVersion = Bundle.main.infoDictionary![kCFBundleVersionKey as String] as! String
         self.view.window?.title = "\(appName) (v\(appVersion))"
@@ -26,26 +27,31 @@ class FixSecuritySettingsVC: NSViewController {
 
         // Build the list of Security Settings for the Main GUI
         for settingToQuery in settingsToQuery {
+            
             let aTaskOutput = runTask(taskFilename: settingToQuery, arguments: ["-a"])  // -a => Applicable given user's OS Version.
             if aTaskOutput == "true" {
-                let dTaskOutput = runTask(taskFilename: settingToQuery, arguments: ["-d"])  // -d => Get Description
-                
-                // Setup Image
+
+                // Setup Status Image
                 let statusImgView = NSImageView(image: NSImage(named: "greyQM")!)
                 statusImgView.identifier = settingToQuery
+                
+                // Setup Setting Description Label
+                let dTaskOutput = runTask(taskFilename: settingToQuery, arguments: ["-d"])  // -d => Get Description
+                let settingDescLabel = NSTextField(labelWithString: dTaskOutput)
 
-                // Setup Button
+                // Setup FixIt Button
                 let fixItBtn = NSButton(title: "Fix It!", target: self, action: #selector(fixItBtnClicked))
                 fixItBtn.identifier = settingToQuery
                 
-                // Setup StackView & Add Image, Label, and Button
+                // Create StackView
                 let entryStackView = NSStackView()  // Default is Horizontal
                 entryStackView.alignment = .centerY
                 entryStackView.spacing = 10
                 entryStackView.distribution = .gravityAreas
 
+                // Add Image, Label, and Button to StackView
                 entryStackView.addView(statusImgView, in: .leading)
-                entryStackView.addView(NSTextField(labelWithString: dTaskOutput), in: .leading)
+                entryStackView.addView(settingDescLabel, in: .leading)
                 entryStackView.addView(fixItBtn, in: .leading)
                 
                 // Add our entryStackView to the settingsStackView
@@ -53,6 +59,7 @@ class FixSecuritySettingsVC: NSViewController {
             }
         }
         
+        // Update all Status Images & FixIt Button visibilities.
         updateAllStatusImagesAndFixItBtns()
     }
     
@@ -68,32 +75,35 @@ class FixSecuritySettingsVC: NSViewController {
     }
     
     func runTask(taskFilename: String, arguments: [String]) -> String {
-        print("runTask: \(taskFilename), with arguments: \(arguments[0])")
-        
         // Note: Running in Main thread because it's not going take long at all (if it does, something is majorly wrong).
+        
+        print("runTask: \(taskFilename) \(arguments[0]) ", terminator: "")  // Finish this print statement at end of runTask() function
+
+        // Make sure we can find the script file. Return if not.
         let settingNameArr = taskFilename.components(separatedBy: ".")
         guard let path = Bundle.main.path(forResource: settingNameArr[0], ofType:settingNameArr[1]) else {
-            print("Unable to locate: \(taskFilename)")
-            return "Unable to locate: \(taskFilename)"
+            print("\n  Unable to locate: \(taskFilename)!")
+            return "Unable to locate: \(taskFilename)!"
         }
         
-        var getDescriptionTask: Process!
-        
-        getDescriptionTask = Process()
-        getDescriptionTask.launchPath = path
-        getDescriptionTask.arguments = arguments
-        
+        // Init outputPipe
         let outputPipe = Pipe()
-        getDescriptionTask.standardOutput = outputPipe
         
-        getDescriptionTask.launch()
-        getDescriptionTask.waitUntilExit()
-        
+        // Setup & Launch our process
+        let ps: Process = Process()
+        ps.launchPath = path
+        ps.arguments = arguments
+        ps.standardOutput = outputPipe
+        ps.launch()
+        ps.waitUntilExit()
+
+        // Read everything the outputPipe captured from stdout
         let data = outputPipe.fileHandleForReading.readDataToEndOfFile()
         var outputString = String(data: data, encoding: String.Encoding.utf8) ?? ""
         outputString = outputString.trimmingCharacters(in: .whitespacesAndNewlines)
         
-        print("  runTask output: \(outputString)")
+        // Return the output
+        print("[output: \(outputString)]")
         return outputString
     }
     
@@ -122,12 +132,14 @@ class FixSecuritySettingsVC: NSViewController {
     func updateAllStatusImagesAndFixItBtns() {
         var allSettingsFixed = true
         
+        // Iterate through all our entryStackViews, finding the image views and buttons.
         for entryStackView in settingsStackView.views as! [NSStackView] {
             if let statusImgView = entryStackView.views.first as! NSImageView? , let fixItBtn = entryStackView.views.last as! NSButton? {
                 let settingToQuery = statusImgView.identifier ?? ""
                 if !settingToQuery.isEmpty {
                     let pfTaskOutput = runTask(taskFilename: settingToQuery, arguments: ["-pf"])  // -pf => Return "pass" or "fail" security test
                     
+                    // Update statusImageView & fixItBtn
                     statusImgView.image = NSImage(named: getImgNameFor(pfString: pfTaskOutput))
                     fixItBtn.isHidden = pfTaskOutput == "pass"
                     
@@ -138,6 +150,7 @@ class FixSecuritySettingsVC: NSViewController {
             }
         }
         
+        // If all settings are fixed, disable the "Fix All" button
         if allSettingsFixed {
             fixAllBtn.isEnabled = false
         }
