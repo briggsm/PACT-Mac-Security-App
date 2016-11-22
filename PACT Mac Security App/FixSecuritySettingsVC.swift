d929440dc75edfe92bdac2c6eaf41e51aeced64b
Mark Briggs <briggsm@alumni.msoe.edu>
2016-11-22 17:50:50 +0300

    Added Localization infrastructure (currently Turkish and Russian). Renamed to 'Security Fixer-Upper'. Added Minimum OS Version (10.8.x) check.

--- PACT Mac Security App/FixSecuritySettingsVC.swift
+++ PACT Mac Security App/FixSecuritySettingsVC.swift
@@ -24,7 +24,20 @@ class FixSecuritySettingsVC: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Ask user their language preference
+        //setGuiLanguage(langInt: langSelectionButtonsAlert())
+        langSelectionButtonsAlert()
+        
+        // Make sure user's OS is Mountain Lion or higher. Mountain Lion (10.8.x) [12.x.x]. If not, tell user & Quit App.
+        let minReqOsVer = OperatingSystemVersion(majorVersion: 10, minorVersion: 8, patchVersion: 0)
+        let userOsVer = ProcessInfo().operatingSystemVersion
+        
+        if !ProcessInfo().isOperatingSystemAtLeast(minReqOsVer) {
+            _ = osVerTooOldAlert(userOsVer: userOsVer)
+            NSApplication.shared().terminate(self)  // Quit App no matter what.
+        }
+        
         // Build the list of Security Settings for the Main GUI
         for settingToQuery in settingsToQuery {
             
@@ -40,7 +53,7 @@ class FixSecuritySettingsVC: NSViewController {
                 let settingDescLabel = NSTextField(labelWithString: dTaskOutput)
 
                 // Setup FixIt Button
-                let fixItBtn = NSButton(title: "Fix It!", target: self, action: #selector(fixItBtnClicked))
+                let fixItBtn = NSButton(title: NSLocalizedString("Fix It!", comment: "button text"), target: self, action: #selector(fixItBtnClicked))
                 fixItBtn.identifier = settingToQuery
                 
                 // Create StackView
@@ -154,5 +167,63 @@ class FixSecuritySettingsVC: NSViewController {
         if allSettingsFixed {
             fixAllBtn.isEnabled = false
         }
+    }
+    
+    func langSelectionButtonsAlert() {
+        let alert: NSAlert = NSAlert()
+        let currLangArr = UserDefaults.standard.value(forKey: "AppleLanguages") as! Array<String>
+        let currLangIso = currLangArr[0]
+        //print("currLangIso: \(currLangIso)")
+        var currLangPretty = ""
+        switch currLangArr[0] {
+        case "tr":
+            currLangPretty = "Türkçe"
+        case "ru":
+            currLangPretty = "Русский"
+        default:
+            currLangPretty = "English"
+        }
+        alert.messageText = "Current Language: \(currLangPretty)"
+        alert.informativeText = "If you choose a DIFFERENT language, this box will disappear and you must RESTART THE APP!"
+        alert.addButton(withTitle: "English")
+        alert.addButton(withTitle: "Türkçe")
+        alert.addButton(withTitle: "Русский")
+       
+        let res = alert.runModal()
+        // Note on res: 1000 => 1st button (on far right), 1001 => 2nd button, 1002 => 3rd, etc
+        switch res {
+        case 1000:  // English
+            if currLangIso != "en" {
+                UserDefaults.standard.setValue(["en"], forKey: "AppleLanguages")
+                UserDefaults.standard.synchronize()
+                NSApplication.shared().terminate(self)
+            }
+        case 1001:  // Turkish
+            if currLangIso != "tr" {
+                UserDefaults.standard.setValue(["tr"], forKey: "AppleLanguages")
+                UserDefaults.standard.synchronize()
+                NSApplication.shared().terminate(self)
+            }
+        case 1002:  // Russian
+            if currLangIso != "ru" {
+                UserDefaults.standard.setValue(["ru"], forKey: "AppleLanguages")
+                UserDefaults.standard.synchronize()
+                NSApplication.shared().terminate(self)
+            }
+        default:
+            // Shouldn't get here
+            break
+        }
+    }
+    
+    func osVerTooOldAlert(userOsVer: OperatingSystemVersion) -> Bool {
+        let alert: NSAlert = NSAlert()
+
+        alert.messageText = NSLocalizedString("Operating System Outdated", comment: "os outdated")
+        alert.informativeText = String.localizedStringWithFormat(NSLocalizedString("Your operating system is too old. It must first be updated to AT LEAST Mountain Lion (10.8) before this app will run. Your OS Version is: [%d.%d.%d]", comment: "os too old message"), userOsVer.majorVersion, userOsVer.minorVersion, userOsVer.patchVersion)
+        
+        alert.alertStyle = NSAlertStyle.informational
+        alert.addButton(withTitle: NSLocalizedString("Quit", comment: ""))
+        return alert.runModal() == NSAlertFirstButtonReturn
     }
 }
