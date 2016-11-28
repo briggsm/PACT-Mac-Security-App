6b702c423074f7eedbf0e6ae4bc28f0cea6eb656
Mark Briggs <briggsm@alumni.msoe.edu>
2016-11-28 23:45:09 +0300

    update mark's restore script

--- PACT Mac Security App/MiscScripts/restoreMarksOrigSettings.sh
+++ PACT Mac Security App/MiscScripts/restoreMarksOrigSettings.sh
@@ -9,6 +9,13 @@ fi
 
 echo
 
+macUUID=`ioreg -rd1 -c IOPlatformExpertDevice | grep -i "UUID" | cut -c27-62`
+if [[ $SUDO_USER != "" ]]; then
+    userOfAdminPriv=$SUDO_USER  # sudo
+else
+    userOfAdminPriv=$USER  # AppleScript 'with administrator privileges'
+fi
+
 # autologindisabled.sh
 echo "* RESTORING: autologindisabled (deleting key: autoLoginUser. removing: /etc/kcpassword)"
 defaults delete /Library/Preferences/com.apple.loginwindow autoLoginUser
@@ -21,8 +28,9 @@ softwareupdate --schedule on
 echo
 
 # bluetoothsharing.sh
-echo "* RESTORING: bluetoothsharing (deleting key: com.apple.Bluetooth PrefKeyServicesEnabled)"
-defaults delete /Library/Preferences/com.apple.Bluetooth PrefKeyServicesEnabled
+echo "* RESTORING: bluetoothsharing (deleting ByHost key: com.apple.Bluetooth PrefKeyServicesEnabled)"
+#defaults delete /Library/Preferences/com.apple.Bluetooth PrefKeyServicesEnabled
+sudo -u $userOfAdminPriv defaults delete /users/$userOfAdminPriv/Library/Preferences/ByHost/com.apple.Bluetooth.$macUUID PrefKeyServicesEnabled
 echo
 
 # ds_store.sh
@@ -60,13 +68,17 @@ echo "* RESTORING: remotedesktopmanagement (kickstart -deactivate -stop)"
 echo
 
 # screensaver5sec.sh
-echo "* RESTORING: screensaver5sec (setting to 5)"
-defaults write com.apple.screensaver askForPasswordDelay -int 5
+echo "* RESTORING: screensaver5sec (askForPassword: 1, askForPasswordDelay: 5)"
+#defaults write com.apple.screensaver askForPasswordDelay -int 5
+sudo -u $userOfAdminPriv defaults write com.apple.screensaver askForPassword -int 1
+sudo -u $userOfAdminPriv defaults write com.apple.screensaver askForPasswordDelay -int 5
 echo
 
 # screensaver10min.sh
 echo "* RESTORING: screensaver10min (setting to 1800)"
-defaults -currentHost write com.apple.screensaver idleTime 1800
+#defaults -currentHost write com.apple.screensaver idleTime 1800
+sudo -u $SUDO_USER defaults write /users/$SUDO_USER/Library/Preferences/ByHost/com.apple.screensaver.$macUUID idleTime 1800
+
 echo
 
 # screenturnoff.sh
